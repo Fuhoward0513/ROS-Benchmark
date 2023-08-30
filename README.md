262f28ff3a90f76eb846fa158d7a83593e32ad55
Fuhoward0513 <zihhaofu@gmail.com>
2023-08-30 16:10:53 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ROS-Benchmark
-Benchmark for ROS's performance
+This is a ROS package using ros-noetic to benchmarking and comparing the performance between different middlewares.
 
 ## Build docker
 ### pull docker image
@@ -7,3 +7,14 @@ Benchmark for ROS's performance
 
 ### create container
     docker run -it -d --name ros-container --rm -v $PWD/ros_ws/:/root/ros_ws osrf/ros:noetic-desktop
+
+
+## Run test
+### Build packages
+    ~# source /opt/ros/noetic/setup.bash 
+    ~/ros_ws# catkin_make
+    ~/ros_ws# source /devel/setup.bash 
+
+### Run  benchmark test
+    roscore
+    ~/ros_ws/src/benchmark/launch# bash run.sh
--- ros_ws/src/benchmark/launch/run.sh
+++ ros_ws/src/benchmark/launch/run.sh
@@ -2,47 +2,8 @@
 
 pub_num_lst=(1 2 5 10)
 sub_num_lst=(1 2 5 10)
-# pub_num_lst=(10)
-# sub_num_lst=(2)
-# fre_lst=(100 50 30 10 1)
-fre_lst=(100 50 30 10)
-# fre_lst=(30)
-# payload_lst=(8 80 200 500 1000 2000 1000000 5000000 10000000 20000000 30000000 50000000)
-# payload_lst=(50000000)
-payload_lst=(8 80 200 500 1000 2000)
-# payload_lst=(1000000 5000000 10000000 20000000 30000000 50000000)
-lag=$((5+5))
-
-for pub_num in "${pub_num_lst[@]}"
-do
-    for sub_num in "${sub_num_lst[@]}"
-    do
-        for fre in "${fre_lst[@]}"
-        do
-            T=$((505/fre+lag))
-            for payload in "${payload_lst[@]}"
-            do
-                for ((i=0; i<pub_num; i++)); do
-                    timeout $T roslaunch benchmark single_pub.launch pub_id:=$i fre:=$fre payload:=$payload sub_num:=$sub_num &
-                    for ((j=0; j<sub_num; j++)); do
-                        timeout $T roslaunch benchmark single_sub.launch pub_id:=$i sub_id:=$j fre:=$fre&
-                        # roslaunch benchmark single_sub.launch pub_id:=$i sub_id:=$j fre:=$fre&
-                    done
-                done
-                ../../system_status/sys_status "$payload" "$fre" "$sub_num" "$pub_num" "$lag"&
-                wait
-                rosrun benchmark file.py $sub_num $pub_num $fre $payload
-            done
-        done
-    done
-done
-
-
-pub_num_lst=(1 2)
-sub_num_lst=(1 2 5 10)
-fre_lst=(100 50 30 10)
-# payload_lst=(8 80 200 500 1000 2000)
-payload_lst=(1000000 5000000 10000000 20000000 30000000 50000000)
+fre_lst=(100 50 30 10 1)
+payload_lst=(8 80 200 500 1000 2000 1000000 5000000 10000000 20000000 30000000 50000000)
 lag=$((5+5))
 
 for pub_num in "${pub_num_lst[@]}"
